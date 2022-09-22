9627d7e1a64f730222e1cbef6a07fc752f6c4a9c
fullstack300622 <108460728+fullstack300622@users.noreply.github.com>
2022-09-22 21:16:13 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+// document.getElementById('myBtn').addEventListener('click',()=>{
+//     document.getElementById('p1').style.color='red';    
+// })
+
+
+// $('#p1').css('color', 'red');
+
+// $('#myBtn').on('click',()=>{
+//     $('#p1').css('color', 'red');
+// })
