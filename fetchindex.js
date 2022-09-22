3548b0068eb653bc91d6a027d08363c9795d0ad3
fullstack300622 <108460728+fullstack300622@users.noreply.github.com>
2022-09-22 21:15:27 +0300

    Create fetchindex.js

---
+++ fetchindex.js
@@ -0,0 +1,26 @@
+// fetch('https://jsonplaceholder.typicode.com/posts')
+// .then((response)=>{
+//     return response.json()
+// })
+// .then((json)=>{
+//     console.log(json)
+// }).catch((err)=>{
+//     console.log(`error ${err}`)
+// })
+
+
+// fetch('https://pokeapi.co/api/v2/berry/')
+//     .then(response => response.json())
+//     .then(json => {
+//         console.log(json)
+//     }).catch((err) => {
+//         console.log(`error ${err}`)})
+
+
+// fetch('https://pokeapi.co/api/v2/berry/')
+//     .then(response => response.json())
+//     .then(json => {
+//         console.log(json)
+//     }).catch((err) => {
+//         console.log(`error ${err}`)
+//     })
